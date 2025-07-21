022440c2ff056864b26c45f57ba9788f54519db7
Usuário <Usurio@DESKTOP-CQB1V0Q>
2025-07-20 21:34:30 -0300

    Classe mãe cliente e filhas PessoaFisica e PessoaJuridica

--- src/loja/model/cliente/PessoaFisica.java
+++ src/loja/model/cliente/PessoaFisica.java
@@ -1,5 +1,18 @@
 package loja.model.cliente;
 
 public class PessoaFisica extends Cliente {
-
+	private String cpf;
+	
+	public PessoaFisica (String nome, String endereco, String telefone, String cpf) {
+		super(nome, endereco, telefone);
+		this.cpf = cpf; 
+	}
+	
+	public String getCpf () {
+		return cpf; 
+	}
+	
+	public void setCpf (String cpf) {
+		this.cpf = cpf;
+	} 
 }
--- src/loja/model/cliente/PessoaJuridica.java
+++ src/loja/model/cliente/PessoaJuridica.java
@@ -1,5 +1,18 @@
 package loja.model.cliente;
 
 public class PessoaJuridica extends Cliente {
-
+private String cnpj;
+	
+	public PessoaJuridica (String nome, String endereco, String telefone, String cnpj) {
+		super(nome, endereco, telefone);
+		this.cnpj = cnpj;
+	}
+	
+	public String getCnpj () {
+		return cnpj;
+	} 
+	
+	public void setCnpj (String cnpj) {
+		this.cnpj = cnpj; 
+	} 
 }
