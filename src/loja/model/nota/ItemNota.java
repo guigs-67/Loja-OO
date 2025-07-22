b0c6868dd71aca2d47828f830c7555c526c16198
AfterDavi <davison.santos@dcomp.ufs.br>
2025-07-21 23:48:32 -0300

    ItemNota.java

--- src/loja/model/nota/ItemNota.java
+++ src/loja/model/nota/ItemNota.java
@@ -1,5 +1,31 @@
 package loja.model.nota;
 
-public class ItemNota extends Nota {
+import java.math.BigDecimal;
+import loja.model.produto;
+import loja.model.cliente;
 
+public class ItemNota{
+    private Produto produto;
+    private int quantidade;
+
+    public ItemNota(Produto produto, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto(){
+        return produto;
+    }
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    public BigDecimal getPrecoFinalItem(){
+        return produto.getPreco().multiply(new BigDecimal(this.quantidade));
+    }
 }
